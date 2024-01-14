65d8220c3e678a9024f0c0b6ee617310a5dcc359
Jeanluc24 <musejnluc02@gmail.com>
2024-01-14 04:54:13 -0800

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,16 +8,15 @@
  * -1 and prints - if number is less than zero
  */
 
-int print_sign(int n)
-{
-    if (n > 0) {
-        _putchar(43);
-        return 1;
-    } else if (n < 0) {
-        _putchar(45);
-        return -1;
-    } else {
-        _putchar(48);
-        return 0;
-    }
+int print_sign(int n) {
+  if (n > 0) {
+    _putchar(43); // '+'
+    return 1;
+  } else if (n < 0) {
+    _putchar(45); // '-'
+    return -1;
+  } else {
+    _putchar(48); // ' '
+    return 0;
+  }
 }
