7eb0a7e4cf26ea7497732cbdb050cf5849feac73
Jeanluc24 <jnlucmusengayezu24@gmail.com>
2024-02-06 08:58:10 -0800

    function that inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,45 +1,51 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked list,
- * at a given position
- * @head: pointer to the pointer to the head of the list.
- * @idx: index where the new node should be added. index starts at 0.
- * @n: value to be assigned to the new node.
- * Return: pointer to the new node, or NULL
+ * insert_nodeint_at_index - Inserts a new node at a given position.
+ * @head: Pointer to the pointer to the head of the list.
+ * @idx: Index where the new node should be added. Index starts at 0.
+ * @n: Value to be assigned to the new node.
+ *
+ * Return: The address of the new node, or NULL if it failed.
  */
-	listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-	{
-	unsigned int count;
-	listint_t *new_node;
-	listint_t *current = *head;
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node, *current;
+	unsigned int count = 0;
+
+	if (head == NULL)
+		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
-	if (!new_node || !head)
-	return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
 	new_node->n = n;
-	new_node->next = NULL;
 
 	if (idx == 0)
 	{
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
 
-	for (j = 0; current && j < idx; j++)
+	current = *head;
+
+	while (current != NULL)
 	{
-	if (j == idx - 1)
-	{
-	new_node->next = current->next;
-	current->next = new_node;
-	return (new_node);
-	}
-	else
-	current = current->next;
+		if (count == idx - 1)
+		{
+			new_node->next = current->next;
+			current->next = new_node;
+			return (new_node);
+		}
+
+		current = current->next;
+		count++;
 	}
 
+	free(new_node);
 	return (NULL);
-	}
+}
