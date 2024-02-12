abe1bc4b739fe02b38d8708927a4654301d44be6
Jeanluc24 <jnlucmusengayezu24@gmail.com>
2024-02-12 13:26:11 -0800

    Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * append_text_to_file - Appending text at the end of a file
+ * @filename: The name of the file.
+ * @text_content: The added content.
+ * Return: 1 if the file exists or -1 if the fails does not exist.
+ */
+	int append_text_to_file(const char *filename, char *text_content)
+	{
+	int count;
+	int letters_wr;
+	int index;
+
+	if (!filename)
+		return (-1);
+
+	count = open(filename, O_WRONLY | O_APPEND);
+
+	if (count == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (letters_wr = 0; text_content[letters_wr]; letters_wr++)
+			;
+
+		index = write(counter, text_content, letters_wr);
+
+		if (index == -1)
+			return (-1);
+	}
+
+	close(count);
+
+	return (1);
+}
